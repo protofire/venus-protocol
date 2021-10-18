b76d13d84270ee06f4e356041cc1fc28e651d81d
lakshmi <defipool@gmail.com>
2021-10-18 18:12:19 +0800

    deploy script for governorAlpha2 contract with reference to proposalCount of GovernorAlpha

--- script/deploy/governor-alpha-2/deploy-governor-alpha-2.js
+++ script/deploy/governor-alpha-2/deploy-governor-alpha-2.js
@@ -1,10 +1,17 @@
 const contractConfigData = require("../../../networks/testnet.json");
 
 (async () => {
+
+  const governorAlphaAddress = contractConfigData.Contracts.GovernorAlpha;
+
+  const governorAlphaContractInstance = await saddle.getContractAt('GovernorAlpha', governorAlphaAddress);
+
+  const lastProposalId = await governorAlphaContractInstance.methods.proposalCount().call();
   const timelockAddress = contractConfigData.Contracts.Timelock;
   const xvsAddress = contractConfigData.Tokens.XVS.address;
   const guardian = contractConfigData.Accounts.Guardian;
-  const constructorArgumentArray = [timelockAddress, xvsAddress, guardian, 100];
+
+  const constructorArgumentArray = [timelockAddress, xvsAddress, guardian, lastProposalId];
   console.log(`Deploying GovernorAlpha2 with timelockAddress, xvsAddress, guardian in constructorArguments: ${constructorArgumentArray}`);
   
   let deployedGovernorAlpha2 = await saddle.deploy('GovernorAlpha2', constructorArgumentArray);
